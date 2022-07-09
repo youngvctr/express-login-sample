6ec387b55eaa2c44fd213bbebdee4c83bd57793e
jmh622 <mh.jung.dev@gmail.com>
2022-07-09 22:47:34 +0900

    form 태그 로그인 샘플 작성

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { engine } from 'express-handlebars';
+import session from 'express-session';
+import flash from 'express-flash';
+import path from 'path';
 
 const app = express();
 const port = 4000;
+const users = [{ id: 'mhjung@sample.com', pw: '12345', name: '명훈' }];
 
 app.engine(
   'hbs',
@@ -18,10 +22,44 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(helmet());
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'jiovfuinewfiuwfe',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
 
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.user;
+  app.locals.signedIn = req.session.signedIn;
+  next();
+});
 app.get('/', (req, res) => res.render('home'));
-app.get('/login', (req, res) => res.render('login'));
+app
+  .route('/signin')
+  .get((req, res) => res.render('signin', { style: 'signin' }))
+  .post((req, res) => {
+    const { id, pw } = req.body;
+    const user = users.find((u) => u.id === id && u.pw === pw);
+    if (user) {
+      req.session.user = user;
+      req.session.signedIn = true;
+      return res.redirect('/');
+    } else {
+      req.flash('error', 'email 또는 password가 잘못 되었습니다.');
+      return res.status(400).render('signin', { style: 'signin' });
+    }
+  });
+app.post('/signout', (req, res) => {
+  req.session.user = null;
+  req.session.signedIn = false;
+  res.redirect('/');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
